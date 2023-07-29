d43a731a005d7f3f0afc588877090caeab951729
SaquibKhan29 <2021032005.saquibkts@student.xavier.ac.in>
2023-07-29 11:44:00 +0530

    Media Query

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:media_query/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+}
